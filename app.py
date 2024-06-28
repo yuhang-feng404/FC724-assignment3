31d94efca91bfcf0b30b54eb10131eb231400a1d
yuhang-feng404 <yuhang-feng404@gmail.com>
2024-06-28 10:33:47 +0800

    Adding html files to the project, and convert the html to flask template format

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def welcome_page():  # put application's code here
+    return render_template('welcome.html')
+
+
+@app.route('/information')
+def information_page():  # put application's code here
+    return render_template('information.html')
+
+
+@app.route('/data_collection')
+def data_collection_page():  # put application's code here
+    return render_template('data_collection.html')
 
 
 if __name__ == '__main__':
