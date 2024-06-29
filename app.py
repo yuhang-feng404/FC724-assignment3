ca5a0e02ce4157c2c3ae898ac7fe4344b8060e71
yuhang-feng404 <yuhang-feng404@gmail.com>
2024-06-29 13:53:49 +0800

    Add flask wtf form, modify the form in data_collection.html

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm, CSRFProtect
+from wtforms import StringField, TextAreaField, SelectField, RadioField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key_here'
+csrf = CSRFProtect(app)
+
+
+class StudentFeedbackForm(FlaskForm):
+    name = StringField('Name:', validators=[DataRequired()])
+    course = StringField('Course:', validators=[DataRequired()])
+    short_answer = TextAreaField('Short-form Answer:', validators=[DataRequired()])
+    long_answer = TextAreaField('Long-form Answer:', validators=[DataRequired()])
+    satisfaction = SelectField('Overall Satisfaction:', choices=[
+        ('', 'Select Satisfaction Level'),
+        ('very-satisfied', 'Very Satisfied'),
+        ('satisfied', 'Satisfied'),
+        ('neutral', 'Neutral'),
+        ('unsatisfied', 'Unsatisfied'),
+        ('very-unsatisfied', 'Very Unsatisfied')
+    ], validators=[DataRequired()])
+    recommend = RadioField('Would you recommend this course to others?', choices=[
+        ('yes', 'Yes'),
+        ('no', 'No')
+    ], validators=[DataRequired()])
+    improvements = TextAreaField('Suggestions for Improvement:')
 
 
 @app.route('/')
@@ -11,11 +37,6 @@ def welcome_page():  # put application's code here
 @app.route('/information')
 def information_page():  # put application's code here
     return render_template('information.html')
-
-
-@app.route('/data_collection')
-def data_collection_page():  # put application's code here
-    return render_template('data_collection.html')
 
 
 @app.route('/current_feedback')
@@ -36,6 +57,23 @@ def current_feedback_submission():
                            satisfaction=satisfaction,
                            recommend=recommend,
                            improvements=improvements)
+
+
+@app.route('/data_collection', methods=['GET', 'POST'])
+def data_collection():
+    form = StudentFeedbackForm()
+    if form.validate_on_submit():
+        # process the form data here
+        name = form.name.data
+        course = form.course.data
+        short_answer = form.short_answer.data
+        long_answer = form.long_answer.data
+        satisfaction = form.satisfaction.data
+        recommend = form.recommend.data
+        improvements = form.improvements.data
+        # do something with the data
+        return 'Thank you for submitting your feedback!'
+    return render_template('data_collection.html', form=form)
 
 
 if __name__ == '__main__':
